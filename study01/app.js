972e9f5c18ae674a81e9f548a1dcb9f15643cfc5
ShellFolder <kk1593@naver.com>
2024-07-05 12:01:06 +0900

    함수 생성 알아보기

--- study01/app.js
+++ study01/app.js
@@ -41,14 +41,74 @@ const test2 = {
 };
 //console.log(test2, test2.c);
 
-console.log(test, test2);
+//console.log(test, test2);
 
 for(a in test2) {
-    console.log(a, test2[a]);
+    //console.log(a, test2[a]);
 }
 
 var {b} = test2;
-console.log("해당 변수명", b);
+//console.log("해당 변수명", b);
 
 var {b:다른이름} = test2;
-console.log("다른 변수명", 다른이름);
+//console.log("다른 변수명", 다른이름);
+
+for(a in test) {
+    //console.log(a, test[a]);
+}
+
+var {b} = test;
+//console.log("해당 변수명", b);
+
+var {b:다른이름} = test;
+//console.log("다른 변수명", 다른이름);
+
+const test3 = {};
+
+test3.a = 1;
+test3.b = 2;
+
+//console.log(test3);
+
+const test4 = test3;
+const test6 = {c: "삼"};
+const test5 = {...test3, ...test6};
+
+//console.log(test4, test5, test5.a);
+
+function fn1() {
+    console.log("fn1");
+}
+
+const fn2 = function() {
+    console.log("fn2");
+}
+
+const fn3 = () => {
+    console.log("fn3");
+}
+const fn4 = () => console.log("fn4");
+/*
+fn1();
+fn2();
+fn3();
+fn4();
+*/
+function fn5(fn) { // 함수 변수를 매개변수로 받을 수 있다. (콜백함수)
+    fn();  // 함수 호출
+}
+fn5(fn1);
+
+fn5(() => {
+    console.log("콜백 함수 내용입니다.");
+});
+
+const page1 = {
+    select: () => console.log("page1.select"),
+    insert: () => {},
+    update: () => {},
+    delete: () => {},
+    selectList: () => {}
+}
+page1.select();
+page1["select"]();
