8719ec9b42fa78e3edd6753aa6040e2a3142e9b1
ShellFolder <kk1593@naver.com>
2024-07-09 11:16:38 +0900

    화면 그리기 2단계

--- study07/app.js
+++ study07/app.js
@@ -19,24 +19,73 @@ import model from "./data1.js";
 
 // 데이터 확인
 //console.log(model);
+/*
 let html = "";
 for(let y = 0; y < model.length; y++) {
-    //console.log(model[y]);
     let temp = "";
     for(let x = 0; x < model[y].length; x++) {
-        //console.log(model[y][x]);
-        // temp += "<div class='"
-        // temp += (model[y][x] == 1) ? "bg1" : "bg2";
-        // temp += "'></div>"
         temp += `<div class="${(model[y][x] == 1) ? "bg1" : "bg2"}"></div>`;
     }
-    //html += temp;
     html += `<li>${temp}</li>`;
-    //console.log(temp);
 }
-//console.log(html);
+
+let html2 = "";
+for(let y = 0; y < model.length; y++) {
+    let temp = "";
+    for(let x = 0; x < model[y].length; x++) {
+        temp += `<div class="${(model[y][x] == 1) ? "bg2" : "bg1"}"></div>`;
+    }
+    html2 += `<li>${temp}</li>`;
+}
+*/
+const dataEvent = (type) => {
+    let html = "";
+    for(let y = 0; y < model.length; y++) {
+        let temp = "";
+        for(let x = 0; x < model[y].length; x++) {
+            if(type) {
+                temp += `<div class="${(model[y][x] == 1) ? "bg1" : "bg2"}"></div>`;
+            } else {
+                temp += `<div class="${(model[y][x] == 1) ? "bg2" : "bg1"}"></div>`;
+            }            
+        }
+        html += `<li>${temp}</li>`;
+    }
+    return html;
+}
 
 const ul = document.getElementsByTagName("ul")[0];
-//console.log(ul);
+//ul.innerHTML = html;
 ul.innerHTML = "";
-ul.innerHTML = html;
+
+const btnEvent = (e) => {
+    //console.log(this, e, e.target, e.target.id);
+    switch (e.target.id) {
+        case "1":
+            ul.innerHTML = "";
+            break;
+        case "2":
+            ul.innerHTML = dataEvent(true);
+            break;
+        default:
+            ul.innerHTML = dataEvent();
+            break;
+    }
+}
+
+const btns = document.getElementsByTagName("button");
+for(let btn of btns) {
+    //console.log(btn);
+    btn.onclick = btnEvent;
+}
+/*
+btns[0].onclick = function() {
+    ul.innerHTML = "";
+}
+btns[1].onclick = function() {
+    ul.innerHTML = html;
+}
+btns[2].onclick = function() {
+    // 기능 부여
+}
+*/
